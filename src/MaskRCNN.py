e11443938a3851fed0478cfaddc56ee985853463
daniel-macrae <d.macrae@student.rug.nl>
2023-01-30 22:20:33 +0100

    first commit
    Upload working model of multi-object tracking and initial object identification using Mask-R-CNN

---
+++ src/MaskRCNN.py
@@ -0,0 +1,36 @@
+from torchvision.transforms import transforms as transforms
+import torch
+import torchvision
+
+
+class MaskRCNN():
+    
+    def __init__(self) -> None:
+        self.model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights=True)
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device(
+            'cpu')
+
+        self.model.eval()
+        self.model.to(self.device)
+
+        self.transform = transforms.Compose([
+        transforms.ToTensor()
+        ])
+
+        self.class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+                    'bus', 'train', 'truck', 'boat', 'traffic light',
+                    'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+                    'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+                    'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+                    'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+                    'kite', 'baseball bat', 'baseball glove', 'skateboard',
+                    'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+                    'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+                    'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+                    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+                    'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+                    'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+                    'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+                    'teddy bear', 'hair drier', 'toothbrush']
+
+
